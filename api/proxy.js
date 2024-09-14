fdb04a53caa87ac876a0600856fd38a7c17e998f
Pablo Santos <51977651+GeekPfs@users.noreply.github.com>
2024-09-13 23:15:56 -0400

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,20 +1,19 @@
 const fetch = require('node-fetch');
 
 export default async function(req, res) {
-  const targetUrl = 'https://standing-gear-ccb.notion.site/101473e7a2bc8043a739c5bc4920943f' + req.url;
+  const targetUrl = 'https://standing-gear-ccb.notion.site/Green-Home-101473e7a2bc8043a739c5bc4920943f' + req.url;
 
   try {
     const response = await fetch(targetUrl);
     const contentType = response.headers.get('content-type');
-    
-    // Define o cabeçalho correto para a resposta
+
+    // Define o tipo correto de conteúdo para o arquivo
     res.setHeader('Content-Type', contentType);
+
     const body = await response.buffer();
-    
-    // Retorna o conteúdo original com o status adequado
     res.status(response.status).send(body);
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Erro ao carregar o conteúdo.');
+    console.error('Erro ao buscar a página:', error);
+    res.status(500).send('Erro no proxy.');
   }
 }
